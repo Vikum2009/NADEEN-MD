fda11b79aa80fb739ca7fd58048cfd78f7942b85
Vikum2009 <nishanthinilminitb@gmail.com>
2025-03-22 08:16:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YsYlRQaa#heVle58By3R4QweJmw8AdzaAAd8wFawfToUY8mKvfcE", //put your session id
+SESSION_ID: process.env.SESSION_ID || "𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=fc5ETKxb#F_UoEfijZMKinuGSgKwjPFuj9R8MqEnSuTW6FEMGUDQ", //put your session id
 MONGODB: process.env.MONGODB || "mongodb://mongo:FQQyixqbcbKXBYexczseeuuIqMXgghuo@nozomi.proxy.rlwy.net:57535",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
 STATUES_REPLY: process.env.STATUES_REPLY || "true", //true or false
 AUTO_REACT: process.env.AUTO_REACT || "true", //true or false
 INBOX_BLOCK: process.env.INBOX_BLOCK || "false", //true or false
-READ_MESSAGE: process.env.READ_MESSAGE || "false", //true or false
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", //true or false
+READ_MESSAGE: process.env.READ_MESSAGE || "true", //true or false
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false", //true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", //true or false
 AUTO_TYPING: process.env.AUTO_TYPING || "false", //true or false
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //true or false
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", //true or false
 AUTO_VOICE:"true" //true or false
 };
